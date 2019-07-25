0acd3a03b3903725f07f8d1355cab2fc0fd484ec
sunxiaoning@shangpoer.com <sunxiaoning@shangpoer.com>
2019-07-25 16:18:45 +0800

    修复图片不显示的问题

---
+++ wetypecho/config.js
@@ -0,0 +1,9 @@
+var domain = "www.sunxiaoning.com"
+var name = "小宁博客"
+var API_SECRET = "www.sunxiaoning.com"
+
+export default {
+    getdomain: domain,
+    getname: name,
+    getapisecret: API_SECRET
+}
